1ceea91ff3ff0a26c602fedf849b9ae57ea2d744
ecbDeveloper <developer.ecb@gmail.com>
2026-06-24 14:22:53 -0300

    chore: improve security group rules from azure

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -63,13 +63,37 @@ resource "azurerm_network_security_group" "sg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "rules"
+    name                       = "allow-http"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-https"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
